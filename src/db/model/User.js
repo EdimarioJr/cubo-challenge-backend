30a80d84e23e5dfdb76c2e5be380cbcdb1e701b0
edimario <edimariobabau@gmail.com>
2020-06-29 17:32:40 -0300

    User Schema

---
+++ src/db/model/User.js
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose'
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    first_name: {
+        type: String,
+        required:[true,'The first name is obrigatory!']
+    },
+    last_name: {
+        type: String,
+        required:[true,'The second name is obrigatory!']
+    },
+    participation: {
+        type: String,
+        required:[true,'The participation name is obrigatory!']
+    }
+})
+
+export default UserSchema;
